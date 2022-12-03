87e065c5c660f1ef1cf08f846feab634c68ac4ad
kalebhenrique <kalebhenriquebr@gmail.com>
2022-12-03 18:28:18 -0300

    :sparkles: model cart created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_03_205120) do
+ActiveRecord::Schema.define(version: 2022_12_03_212734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2022_12_03_205120) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_carts_on_product_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -55,6 +64,8 @@ ActiveRecord::Schema.define(version: 2022_12_03_205120) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "carts", "products"
+  add_foreign_key "carts", "users"
   add_foreign_key "products", "brands"
   add_foreign_key "products", "categories"
 end
---
+++ spec/factories/carts.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :cart do
+    product { nil }
+    user { nil }
+  end
+end
