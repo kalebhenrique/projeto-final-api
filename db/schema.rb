7533a74ab385becc1e45d1745a61768d91e13960
kalebhenrique <kalebhenriquebr@gmail.com>
2022-12-03 18:20:53 -0300

    user factory updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_03_193152) do
+ActiveRecord::Schema.define(version: 2022_12_03_205120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,21 @@ ActiveRecord::Schema.define(version: 2022_12_03_193152) do
     t.integer "price_in_cents"
     t.index ["brand_id"], name: "index_products_on_brand_id"
     t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "credit"
+    t.boolean "is_admin"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "products", "brands"
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,14 @@
 FactoryBot.define do
+  sequence(:user_emails) { |n| "email#{n}@gmail" }
+
   factory :user do
-    name { "MyString" }
+    email { generate(:user_emails) }
+    name { "name" }
+    password { 123456 }
     credit { 1 }
     is_admin { false }
+    trait :admin do
+      is_admin { true }
+    end
   end
 end
