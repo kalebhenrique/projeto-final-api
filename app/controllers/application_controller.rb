f96b5653978b9b1255a493dedf12787142af3016
jessica8melo <jessica8melo@gmail.com>
2022-12-06 22:03:11 -0300

    ✨ user login and admin authentication created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    def require_login
+        head(:unauthorized) unless current_user.presence
+    end
+
+    def authentication_admin
+      head(:bad_request) unless current_user.presence && current_user.is_admin
+    end
 end
