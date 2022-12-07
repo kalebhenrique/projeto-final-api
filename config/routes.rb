421a597bdd0358b0b436eae6b9ca1bfb64d38bd4
jessica8melo <jessica8melo@gmail.com>
2022-12-06 22:01:40 -0300

    users, brands routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: :all
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace 'api' do
+    namespace 'v1' do
+      scope 'users' do
+        get 'login', to: 'users#login'
+        get 'logout', to: 'users#logout'
+      end
+      scope 'brands' do
+        get 'index', to: 'brands#index'
+        get 'show/:id', to: 'brands#show'
+        post 'create', to: 'brands#create'
+        patch 'update/:id', to: 'brands#update'
+        delete 'delete/:id', to: 'brands#delete'
+      end
+    end
+  end
 end
